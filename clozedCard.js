286f141eb6f878328e44f4fa935f69ec6144f9ff
EliseoMejia <eliseofmejia@gmail.com>
2017-09-06 18:29:56 -0400

    clozeCard.js partialDisplay works and replaces strings input

--- clozedCard.js
+++ clozedCard.js
@@ -1,4 +1,5 @@
 //constructor to build cloze flash cards
+var blank = function(){console.log("________________")};
 
 var ClozeCard= function(Front, Back){
 	if(this instanceof ClozeCard){
@@ -16,6 +17,16 @@ var ClozeCard= function(Front, Back){
 			}else{
 				console.log("Cloze is not part of Full. Make sure cloze matches with full");
 			}
+		};
+		this.displayPartial = function () {
+			var string = this.front;
+			//console.log(string);
+			//blank();
+			var repl = this.back;
+			//console.log(repl);
+			//blank();
+			var partialString = string.replace(repl,"_____________");
+			console.log(partialString);
 		}
 	}else{
 		return new ClozeCard(Front, Back);
@@ -29,12 +40,13 @@ var flash = new ClozeCard("Samsung is a Korean Electronics Brand", "Samsung");
 flash.displayFull();
 flash.displayCloze();
 flash.checkStrings();
+flash.displayPartial();
 
 //checking when strings aren't like
 
-console.log("------------------");
-var flash2 = new ClozeCard("Apple is an american electronics brand", "So is MicroSoft");
+// console.log("------------------");
+// var flash2 = new ClozeCard("Apple is an american electronics brand", "So is MicroSoft");
 
-flash2.displayFull();
-flash2.displayCloze();
-flash2.checkStrings();
+// flash2.displayFull();
+// flash2.displayCloze();
+// flash2.checkStrings();
